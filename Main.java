17aee6d21047e3243a825c336571a97368101a38
Furkan Türkyaşar <53195046+furkanturkyasar@users.noreply.github.com>
2022-01-19 18:56:01 +0300

    Main class

---
+++ Main.java
@@ -0,0 +1,12 @@
+public class Main{
+    public static void main(String[] args) {
+        Employee e1 = new Employee("Furkan",2000.0,45,1985);
+        Employee e2 = new Employee("Ahmet", 900.0, 48, 2017);
+
+        e1.Printing();
+        System.out.println("--------------");
+        //e2.Printing();
+
+    }
+
+}
